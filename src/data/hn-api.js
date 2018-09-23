77f69482fd2eaec652243895310f1e66e496130a
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2018-09-24 00:34:01 +0300

    Add polymer-cli dependency

--- src/data/hn-api.js
+++ src/data/hn-api.js
@@ -1,4 +1,4 @@
-const HN_API = "http://node-hnapi.herokuapp.com/news";
+const HN_API = "https://node-hnapi.herokuapp.com/news";
 export function fetchTopNews(page) {
     page = page || 1;
     return fetch(`${HN_API}?page=${page}`)
