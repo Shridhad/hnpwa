9f768318956db0d4bb46c593be4834d85c30b0ca
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2018-09-24 00:33:06 +0300

    Git ignore build folder

--- build/default/src/data/hn-api.js
+++
@@ -1,5 +0,0 @@
-const HN_API = "http://node-hnapi.herokuapp.com/news";
-export function fetchTopNews(page) {
-  page = page || 1;
-  return fetch(`${HN_API}?page=${page}`).then(response => response.json());
-}
